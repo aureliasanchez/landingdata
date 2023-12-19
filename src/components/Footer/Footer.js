5eb2fa0c958977c4e9b5e882cc820a502b84ca1a
unknown <aureliasancheze5@gmail.com>
2023-12-18 18:10:23 -0800

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import AUX from '../../hoc/Aux_';
 import { Link } from 'react-router-dom';
+import { FaFacebookSquare, FaTwitterSquare, FaLinkedin, FaInstagramSquare, FaTiktok } from 'react-icons/fa';
 
 class Footer extends Component{
     
@@ -74,19 +75,41 @@ class Footer extends Component{
         return(
             <AUX>
                 <section className="pt-5 bg-dark" id="contact">
-                <div className="container">
-                    
-                    <div className="row mt-5 pt-5 pb-2">
-                        <div className="col-md-12">
-                            <div className="text-white footer-alt">
-                                <div className="float-left">
-                                    <p className="copyright-desc pb-0">2023 DataAlpine. Todos los derechos reservados</p>
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <div className="row mt-5 pt-5 pb-2">
+      <div className="col-md-6">
+        <div className="text-white pl-4 pr-4">
+          <p className="copyright-desc pb-0">2023 DatAlpine. Todos los derechos reservados</p>
+        </div>
+      </div>
+      <div className="col-md-6 d-flex justify-content-end align-items-center">
+        {/* Facebook */}
+        <a href="https://www.facebook.com/profile.php?id=61552173002247" target="_blank" rel="noopener noreferrer" className="ml-2">
+          <FaFacebookSquare size={20} color="white" />
+        </a>
+
+        {/* Twitter */}
+        <a href="https://twitter.com/DatAlpine" target="_blank" rel="noopener noreferrer" className="ml-2">
+          <FaTwitterSquare size={20} color="white" />
+        </a>
+
+        {/* LinkedIn */}
+        <a href="https://www.linkedin.com/company/datalpine/" target="_blank" rel="noopener noreferrer" className="ml-2">
+          <FaLinkedin size={20} color="white" />
+        </a>
+
+        {/* Instagram */}
+        <a href="https://www.instagram.com/DatAlpine/?fbclid=IwAR0B-HYWqGM5of0fBiCHFVWD7Uysb-punr5kVR8JHI11qobuvOzIMdJ6w14" target="_blank" rel="noopener noreferrer" className="ml-2">
+          <FaInstagramSquare size={20} color="white" />
+        </a>
+
+        {/* TikTok */}
+        <a href="https://www.tiktok.com/@datalpine?_t=8gsoksxpqdz&_r=1" target="_blank" rel="noopener noreferrer" className="ml-2">
+          <FaTiktok size={20} color="white" />
+        </a>
+      </div>
+    </div>
+                
+                  
             </section> 
             </AUX>
         );
--- src/containers/MainContent/Sections/Pricing.js
+++ src/containers/MainContent/Sections/Pricing.js
@@ -141,7 +141,7 @@ class Pricing extends Component{
 
                                     <div className="mt-5 mb-20 d-flex justify-content-center align-items-center" style={{ marginBottom: '50px' }}>
                                     
-                                        <Link to="contacto@datalpine.mx" className="btn btn-custom btn-rounded" style={{ marginTop: '18px', marginBottom: '20px' }}>Contacto con ventas</Link>
+                                        <Link to="contacto_ventas" className="btn btn-custom btn-rounded" style={{ marginTop: '18px', marginBottom: '20px' }}>Contacto con ventas</Link>
                                     </div>
 
                                 </div>
